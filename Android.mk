d5a839c6f08854dd184f5520653900dbaa1658e1
bauner <bauner@gmx.de>
2016-01-17 15:26:02 +0100

    merged cm changes

--- Android.mk
+++ Android.mk
@@ -32,8 +32,10 @@ endif
 # Wifi
 ifeq ($(BOARD_HAVE_SAMSUNG_WIFI),true)
 include $(SAM_ROOT)/macloader/Android.mk
+include $(SAM_ROOT)/wifiloader/Android.mk
 endif
 
 ifeq ($(BOARD_VENDOR),samsung)
+include $(SAM_ROOT)/modemloader/Android.mk
 include $(SAM_ROOT)/ril/Android.mk
 endif
